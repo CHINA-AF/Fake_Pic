9c77b051092b910d8c6fbce89ddea44a52cbbf2a
龙.吟 <1293672598@qq.com>
2021-12-09 14:57:41 +0800

    增加进度条

--- Huanying_Tank.py
+++ Huanying_Tank.py
@@ -6,6 +6,17 @@ import numpy as np
 import time
 
 
+def process_bar(percent, total_length=15):
+    """
+    进度条
+    :param percent: 当前进度
+    :param total_length: 进度条长度
+    :return:
+    """
+    bar = '[' + '*' * int(percent * total_length) + '-' * (total_length - int(percent * total_length)) + ']'
+    print('\r%s %d%%' % (bar, percent * 100), end='')
+
+
 def main(pic1, pic2, result_name):
     pic1 = Image.open(pic1).convert("L")
     pic2 = Image.open(pic2).convert("L")
@@ -13,18 +24,18 @@ def main(pic1, pic2, result_name):
     arr2 = np.array(pic2)
     height = min(arr1.shape[0], arr2.shape[0])
     width = min(arr1.shape[1], arr2.shape[1])
-    result = np.zeros((height,width,4),np.uint8)
+    result = np.zeros((height, width, 4), np.uint8)
     for h in range(0, height):
-        row = []
+        process_bar(h / (height - 1), total_length=20)
         for w in range(0, width):
             a = arr1[h, w] / 255  # A=α·X+(1-α)·白色背景
-            b = arr2[h, w] / 255 # B=α·X+(1-α)·黑色背景
+            b = arr2[h, w] / 255  # B=α·X+(1-α)·黑色背景
             alpha = 1 - (a - b)
-            g = int((b / alpha)*255)
+            g = int((b / alpha) * 255)
             if alpha > 1:
                 alpha = 1
             point = [g, g, g, alpha * 255]
-            result[h,w] = point
+            result[h, w] = point
     result = np.array(result)
     result_pic = Image.fromarray(np.uint8(result))
     result_pic.save(result_name)
@@ -32,11 +43,12 @@ def main(pic1, pic2, result_name):
 
 
 if __name__ == '__main__':
-    path1 = input('白色背景图片：')
-    path2 = input('黑色背景图片：')
-    path3 = input('输出文件名：')
-    # t1 = time.time()
-    # main('pic1.jpg','pic2.jpg','t.png')
-    # t2 = time.time()
-    # print(t2-t1)
+    # path1 = input('白色背景图片：')
+    # path2 = input('黑色背景图片：')
+    # path3 = input('输出文件名：')
+    # main(path1,path2,path3)
+    t1 = time.time()
+    main('pic1.jpg', 'pic2.jpg', 't.png')
+    t2 = time.time()
+    print(t2 - t1)
     # 8.604106903076172s
